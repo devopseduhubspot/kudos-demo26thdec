fa69dc970f6e56e7ff975a0faad3ae94ee581da7
Showkath Ali <showkathali@example.com>
2025-12-27 09:28:12 +0530

    Added usesate in first line import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react'; //  useState unused
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ const appName = "Kudos App"; //  unused variable
+
+ ReactDOM.createRoot(document.getElementById('root')).render(
+ <React.StrictMode>
+      <App />
+</React.StrictMode>
 );
